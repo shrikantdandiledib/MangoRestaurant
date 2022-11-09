16f887f9a43295d3b1f9cc07d7cfcbb5168c2fe5
Shrikant Dandile <94945253+shrikantdandiledib@users.noreply.github.com>
2022-11-09 08:16:17 +0530

    Product DB Created

---
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContexts.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Mango.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContexts:DbContext
+    {
+        public ApplicationDbContexts(DbContextOptions<ApplicationDbContexts> options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,13 +1,19 @@
+using Mango.Services.ProductAPI.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add Database Connection
+builder.Services.AddDbContext<ApplicationDbContexts>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductDB")));
+var app = builder.Build();
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
